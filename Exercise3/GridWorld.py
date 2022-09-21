5e4e1fc683a18dba8c36db60233fff0936d1e994
Viktor Bjerg Nordstrøm <vinor19@student.sdu.dk>
2022-09-21 12:25:04 +0200

    Changed GridWorld to work with earlier version of python

--- Exercise3/GridWorld.py
+++ Exercise3/GridWorld.py
@@ -1,31 +1,23 @@
-from tkinter import Grid
 import numpy
-
 
 class GridWorld:
     def __init__(self):
-        self.board = numpy.array(16)
         self.exits = [0,15]
         self.position = 6
 
     def move(self, direction):
-        match direction:
-            case 0:
-                self.position-=4
-                if self.position < 0:
-                    self.position+=4
-            case 1:
-                self.position+=1
-                if self.position % 4 == 0:
-                    self.position-=1
-            case 2:
-                self.position+=4
-                if self.position > 15:
+        if direction == 0:
+                if self.position-4 >= 0:
                     self.position-=4
-            case 3:
-                self.position-=1
-                if self.position % 4 == 3:
+        elif direction == 1:
+                if self.position % 4 != 0:
                     self.position+=1
+        elif direction == 2:
+                if self.position <= 15:
+                    self.position+=4
+        elif direction == 3:
+                if self.position % 4 != 3:
+                    self.position-=1
 
     def over(self):
         return self.position in self.exits
