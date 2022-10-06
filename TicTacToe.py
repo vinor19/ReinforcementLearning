7100a987c245d60620f79ca3be1ce4eef16acec7
Viktor <thedarkner9000@gmail.com>
2022-10-06 17:58:11 +0200

    Updated training, got a better expert

--- TicTacToe.py
+++ TicTacToe.py
@@ -5,6 +5,7 @@ import copy as cp
 import json
 import matplotlib
 from matplotlib import pyplot as plt
+from multiprocessing import Process
 
 class TicTacToe:
     def __init__(self):
@@ -54,9 +55,8 @@ def getAvailableMoves(game):
             possibilities.append(i)
     return possibilities
 
-def minimax(board, alpha, beta, maximizing_player):
+def minimax(board, depth, alpha, beta, maximizing_player):
     children = getAvailableMoves(board)
-    depth = len(children)-1
     if depth == -1:
         return 0, evaluate(board)
     if depth == 0 or board.GameOver()[0]:
@@ -69,7 +69,7 @@ def minimax(board, alpha, beta, maximizing_player):
         for child in children:
             board_copy = cp.deepcopy(board)
             board_copy.move(child)
-            current_eval = minimax(board_copy, alpha, beta, False)[1]
+            current_eval = minimax(board_copy, depth-1, alpha, beta, False)[1]
             if current_eval > max_eval:
                 max_eval = current_eval
                 best_move = child
@@ -83,7 +83,7 @@ def minimax(board, alpha, beta, maximizing_player):
         for child in children:
             board_copy = cp.deepcopy(board)
             board_copy.move(child)
-            current_eval = minimax(board_copy, alpha, beta, True)[1]
+            current_eval = minimax(board_copy, depth-1, alpha, beta, True)[1]
             if current_eval < min_eval:
                 min_eval = current_eval
                 best_move = child
@@ -115,7 +115,7 @@ def startExpert2(game):
             piecePlayed, _ = game.move(int(input('Where do you place your piece:\n'))-1)
         else:
             print(getAvailableMoves(game))
-            piece, _ = minimax(game, np.NINF, np.Inf, player == 1)
+            piece, _ = minimax(game, len(getAvailableMoves(game))-1, np.NINF, np.Inf, player == 1)
             print(piece)
             piecePlayed, _ = game.move(piece)
         depth -= 1
@@ -165,36 +165,32 @@ def startAIvsAIGame(ai, ai2, game  = TicTacToe(), printStates = False):
     return winner
 
 def startAIGamevsExpert(ai, game  = TicTacToe(), p = 1):
-    global state
     game = game
     gameover = False
     winner = 0
     player = p
     while not gameover:
-        print("---------------")
-        print(game.board[0:3])
-        print(game.board[3:6])
-        print(game.board[6:9])
+        # print("---------------")
+        # print(game.board[0:3])
+        # print(game.board[3:6])
+        # print(game.board[6:9])
         if game.CurrentPlayer == player:
-            piece, _ = minimax(game, np.NINF, np.Inf, p == 2)
-            print("Expert placed at: " + str(piece))
-
-            game.move(piece)
+            game.move(minimax(game, len(getAvailableMoves(game))-1, np.NINF, np.Inf, True)[0])
         else:
-            piece = ai.chooseAction(game)
-            print("AI placed at: " + str(piece))
-            game.move(piece)
+            game.move(ai.chooseAction(game))
         gameover, winner = game.GameOver()
-        state = game.board
     return winner
+
+
 
 def boardToState(board):
     output = ""
     for i in board:
         output += str(i)
     return output
-f = open("Tree.json", "r")
-aiTree = json.load(f).keys()
+
+# f = open("Tree.json", "r")
+# aiTree = json.load(f).keys()
 
 class TictactoeAI:
     def __init__(self, gamma = 0.9, theta = 0.01, game = TicTacToe(), lr = 0.2, epsilon = 0.3, N = 1):
@@ -205,7 +201,6 @@ class TictactoeAI:
         self.theta = theta
         self.game = game
         self.N = N
-        #Initialize Q_ø
 
     def chooseActionTraining(self, game):
         boardHash = boardToState(game.board)
@@ -237,7 +232,7 @@ class TictactoeAI:
         else:
             return 0
 
-    def train(self,againstExpert = False,againstSelf = True, epochs = 100, episodes = 100):
+    def train(self,againstExpert = False, againstSelf = False, epochs = 100, episodes = 100):
         for i in range(epochs):
             if againstExpert:
                 self.learnvsexpert(episodes, p=1)
@@ -253,7 +248,7 @@ class TictactoeAI:
             #     print(str(i/episodes*100)+"%")
             game = cp.deepcopy(self.game)
             if (p == 2):
-                game.move(minimax(game, np.NINF, np.Inf, p==2)[0])
+                game.move(minimax(game, len(getAvailableMoves(game))-1, np.NINF, np.Inf, p==2)[0])
             while not game.GameOver()[0]:
                 action0 = None
                 state0 = None
@@ -263,7 +258,7 @@ class TictactoeAI:
                 for i in range(0,self.N):
                     action = self.chooseActionTraining(nextgamestate)
                     reward1, _ = nextgamestate.move(action)
-                    actionExpert, _ = minimax(nextgamestate, np.NINF, np.Inf, p==2)
+                    actionExpert, _ = minimax(nextgamestate, len(getAvailableMoves(game))-1, np.NINF, np.Inf, p==2)
                     reward2, _ = nextgamestate.move(actionExpert)
                     if i == 0:
                         action0 = action
@@ -278,7 +273,6 @@ class TictactoeAI:
                 self.Q_ø[boardToState(state0)][action0] = newValue
                 game.move(action0)
                 game.move(actionOpponent)
-        # print("100%")
 
     def learnvsself(self, episodes, p = 1):
         # converged = False
@@ -296,12 +290,15 @@ class TictactoeAI:
                 nextgamestate = cp.deepcopy(game)
                 for i in range(0,self.N):
                     action = self.chooseActionTraining(nextgamestate)
-                    reward1, _ = nextgamestate.move(action)
-                    actionOldVersion = self.chooseAction(nextgamestate)
-                    reward2, _ = nextgamestate.move(actionOldVersion)
+                    _, reward1 = nextgamestate.move(action)
+                    actionOther = self.chooseAction(nextgamestate)
+                    _, reward2 = nextgamestate.move(actionOther)
+                    if nextgamestate.GameOver()[0] and nextgamestate.GameOver()[1]  == 0:
+                        reward1 = 5
+                        reward2 = 0
                     if i == 0:
                         action0 = action
-                        actionOpponent = actionOldVersion
+                        actionOpponent = actionOther
                         state0 = boardToState(game.board)
                         self.Q_ø[boardToState(state0)] = self.Q_ø.get(boardToState(game.board),np.zeros(9).tolist())
                     giN += self.gamma**i * (reward1-reward2)
@@ -312,10 +309,90 @@ class TictactoeAI:
                 self.Q_ø[boardToState(state0)][action0] = newValue
                 game.move(action0)
                 game.move(actionOpponent)
-        # print("100%")
         
-aivsexpert = TictactoeAI(N=3, lr=0.01)
-aivsself = TictactoeAI(N=3, lr=0.01)
+""" MATHIAS EXPERT """
+f = open("MathExpert.json","r")
+mathExpert = json.load(f)
+f.close()
+def printBoard(board):
+    print(board[0:3])
+    print(board[3:6])
+    print(board[6:9])
+
+def startAIGamevsMATH(ai, game  = TicTacToe(), p = 1, doPrint = False):
+    game = game
+    gameover = False
+    winner = 0
+    player = p
+    if doPrint:
+        printBoard(game.board)
+    while not gameover:
+        if game.CurrentPlayer == player:
+            move = mathMoves[mathExpert[toState(game.board)]]
+            game.move(move)
+            if doPrint:
+                print("---------------")
+                printBoard(game.board)
+                print("Expert moved")
+        else:
+            move = ai.chooseAction(game)
+            game.move(move)
+            if doPrint:
+                print("---------------")
+                printBoard(game.board)
+                print("AI moved")
+        gameover, winner = game.GameOver()
+        
+    return winner
+
+def startvsMATH(ai, game  = TicTacToe(), p = 2, doPrint = False):
+    game = game
+    gameover = False
+    winner = 0
+    player = p
+    if doPrint:
+        printBoard(game.board)
+    while not gameover:
+        if game.CurrentPlayer == player:
+            move = mathMoves[mathExpert[toState(game.board)]]
+            game.move(move)
+            if doPrint:
+                print("---------------")
+                printBoard(game.board)
+                print("Expert moved")
+        else:
+            game.move(int(input('Where do you place your piece:\n')))
+            if doPrint:
+                print("---------------")
+                printBoard(game.board)
+                print("AI moved")
+        gameover, winner = game.GameOver()
+        
+    return winner
+
+def toState(board: list) -> int:
+        """Get the board state
+
+        Returns:
+            int: A unique integer identifying the current state
+
+        Note that the state is calculated differently based on whose turn it currently is.
+        Essentially, the state is represented as a number in base 3.
+        The current player is represented as a "1" in the number, and the opposite player is "2".
+        Empty is "0".
+
+        """
+        empty_spots = board.count(0)
+        val = 0
+        for x in board:
+            val = val * 3 + (0
+                             if x == 0
+                             else (x + empty_spots) % 2 + 1)
+        return str(int(val))
+
+mathMoves = {"NW": 0, "N": 1, "NE": 2, "W":3, "C": 4, "E": 5, "SW": 6, "S": 7, "SE": 8}
+
+
 
 
 # f = open("AI.json","w")
@@ -324,44 +401,69 @@ aivsself = TictactoeAI(N=3, lr=0.01)
 # f = open("AI.json","r")
 # f.close()
 # print(len(ai.Q_ø.keys()))
-scoresSelf = []
-scoresExpert = []
-for i in range(500):
-    Wins = [0,0]
-    aivsexpert.train(againstExpert = True, againstSelf = False, epoch = 1, episodes=5)
-    aivsself.train(epochs=5, episodes=1)
-    for j in range(1):
-        nextgame = TicTacToe()
-        winner = startAIvsAIGame(aivsexpert, aivsself, game=nextgame)
-        if winner == 2:
-            Wins[1] += 1
-        elif winner == 1:
-            Wins[0] +=1
-    for j in range(1):
-        nextgame = TicTacToe()
-        winner = startAIvsAIGame(aivsself, aivsexpert, game=nextgame)
-        if winner == 1:
-            Wins[1] += 1
-        elif winner == 2:
-            Wins[0] +=1
-    scoresExpert.append(Wins[0])
-    scoresSelf.append(Wins[1])
-    if i%50 == 50:
-        _, ax = plt.subplots()
-        line1 = ax.plot(scoresExpert)
-        line2 = ax.plot(scoresSelf)
-        plt.xlabel("per 5 Epoch")
-        plt.ylabel("Wins")
-        plt.title("Training methods")
-        ax.legend(["Expert Training","Self training"])
-        plt.show()
-        print(scoresExpert,scoresSelf)
-_, ax = plt.subplots()
-line1 = ax.plot(scoresExpert)
-line2 = ax.plot(scoresSelf)
-plt.xlabel("per 5 Epoch")
-plt.ylabel("Wins")
-plt.title("Training methods")
-ax.legend(["Expert Training","Self training"])
-plt.show()
-print(Wins)
+
+# scoresExpert = np.zeros(rounds).tolist()
+# draws = np.zeros(rounds).tolist()
+def testTraining(ai, scores: list, rounds, tests):
+    for j in range(tests):
+        aiInTraining = cp.deepcopy(ai)
+        for i in range(rounds):
+            Wins = [0,0,0]
+            # aivsexpert.train(againstExpert = True, epoch = 1, episodes=5)
+            aiInTraining.train(againstSelf = True, epochs=5, episodes=5)
+            nextgame = TicTacToe()
+            winner = startAIGamevsMATH(aiInTraining, game=nextgame, p=2)
+            if winner == 2:
+                Wins[1] += 1
+            elif winner == 1:
+                Wins[0] += 1
+            else:
+                Wins[2] += 1
+            nextgame = TicTacToe()
+            winner = startAIGamevsMATH(aiInTraining, game=nextgame, p=1)
+            if winner == 1:
+                Wins[1] += 1
+            elif winner == 2:
+                Wins[0] +=1
+            else:
+                Wins[2] += 1
+            # scoresExpert[i]+=Wins[0]/tests
+            scores[i]+=(Wins[0]-Wins[1]+Wins[2]/2)/tests
+            # draws[i]+=Wins[2]/tests
+        print(j)
+
+
+if __name__ == '__main__':
+    rounds = 500
+    tests = 5
+    scoresN1 = np.zeros(rounds).tolist()
+    scoresN2 = np.zeros(rounds).tolist()
+    scoresN3 = np.zeros(rounds).tolist()
+    aivsn1 = TictactoeAI(N=1, lr=0.05)
+    aivsn2 = TictactoeAI(N=2, lr=0.05)
+    aivsn3 = TictactoeAI(N=3, lr=0.05)
+    # p1 = Process(target=testTraining, args=(aivsn1, scoresN1, rounds, tests))
+    # p1.start()
+    # p2 = Process(target=testTraining, args=(aivsn2, scoresN2, rounds, tests))
+    # p2.start()
+    # p3 = Process(target=testTraining, args=(aivsn3, scoresN3, rounds, tests))
+    # p3.start()
+    # p1.join()
+    # p2.join()
+    # p3.join()
+    testTraining(aivsn1, scoresN1, rounds=rounds, tests=tests)
+    testTraining(aivsn2, scoresN2, rounds=rounds, tests=tests)
+    testTraining(aivsn3, scoresN3, rounds=rounds, tests=tests)
+    _, ax = plt.subplots()
+    # line1 = ax.plot(scoresExpert)
+    line2 = ax.plot(scoresN1)
+    line2 = ax.plot(scoresN2)
+    line2 = ax.plot(scoresN3)
+    # line3 = ax.plot(draws)
+    plt.xlabel("per 5 Epoch of 5 training games")
+    plt.ylabel("Wins")
+    plt.title("Training methods")
+    ax.legend(["ScoreN1","ScoreN2","ScoreN3"])
+    # plt.show()
+    plt.savefig('foo.png')
+
